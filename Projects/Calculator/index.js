62c7536b3ec9da3cee7bba83d0b07aa7ef1e2dd3
Tufail Ahmad <tufailahmadashrafi@gmail.com>
2025-03-19 19:46:55 +0530

    calculator all functionality complete

--- Projects/Calculator/index.js
+++ Projects/Calculator/index.js
@@ -1,42 +1,57 @@
 /* darkMode Functionality */
-let darkmode = localStorage.getItem('darkmode');
+let darkmode = localStorage.getItem("darkmode");
 
-const toggleBtn = document.querySelector('#toggle-btn');
+const toggleBtn = document.querySelector("#toggle-btn");
 
 const enableDarkMode = () => {
-  document.body.classList.add('darkmode');
-  localStorage.setItem('darkmode','active');
-}
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+};
 
 const disableDarkMode = () => {
-  document.body.classList.remove('darkmode');
-  localStorage.setItem('darkmode',null);
-}
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
+};
 
-toggleBtn.addEventListener('click', () => {
-  darkmode = localStorage.getItem('darkmode')
-  darkmode !== "active" ? enableDarkMode() : disableDarkMode()
-})
+if (darkmode == "active") enableDarkMode();
+
+toggleBtn.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkMode() : disableDarkMode();
+});
 
 /* Calculator Functionality */
-let displayValue = '';
-document.querySelector('.display').value = displayValue;
-document.querySelector('.display2').value = displayValue;
-let button = document.querySelectorAll(".btn");
+let input1 = document.querySelector(".display");
+let input2 = document.querySelector(".display2");
+let buttons = document.querySelectorAll(".btn");
+let allClearBtn = document.querySelector("#all-clear");
+let backspaceBtn = document.querySelector("#backspace-btn");
+let displayValue = "";
 
-button.addEventListener('click', () => {
-  if (button === '='){
-    displayValue = eval(displayValue);
-    document.querySelector('.display').value = displayValue;
-    document.querySelector('.display2').value = ''
-  } else if (true ) {
-    
-  }
-  
-})
+console.log(buttons);
+let arr = Array.from(buttons);
+// console.log(arr);
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML === "=") {
+      displayValue = eval(displayValue);
+      input1.value = displayValue;
+      input2.value = "";
+    } else {
+      displayValue += e.target.innerHTML;
+      input2.value = displayValue;
+      input1.value = "";
+    }
+  });
+});
 
-function allClear() {
-  displayValue = '';
-  document.querySelector('.display2').value = displayValue;
-  document.querySelector('.display  ').value = displayValue;
-};
+allClearBtn.addEventListener("click", (e) => {
+  displayValue = "";
+  input1.value = displayValue;
+  input2.value = displayValue;
+});
+
+backspaceBtn.addEventListener("click", (e) => {
+  displayValue = displayValue.substring(0, displayValue.length - 1);
+  input2.value = displayValue;
+});
