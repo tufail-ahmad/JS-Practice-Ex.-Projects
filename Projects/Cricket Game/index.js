347bbb824e86fa0fab44854a5c0708ccb15b3bf1
Tufail Ahmad <tufailahmadashrafi@gmail.com>
2025-04-08 22:58:28 +0530

    modify cricket game project

--- Projects/Cricket Game/index.js
+++ Projects/Cricket Game/index.js
@@ -1,3 +1,16 @@
+/*==================== Score Functionality =====================*/
+let scoreStr = localStorage.getItem("score");
+
+let score = JSON.parse(scoreStr) || {
+  win: 0,
+  lose: 0,
+  tie: 0,
+};
+
+let displayScore = () => {
+  return `win: ${score.win}, lost: ${score.lose}, tie: ${score.tie}`;
+};
+
 /*==================== Computer Random Move =====================*/
 let computerMove;
 const computerRandomMove = () => {
@@ -11,26 +24,29 @@ const computerRandomMove = () => {
   }
 };
 
-/*==================== Result Msg Functionality =====================*/
-let resultMsg;
-const result = () => {
-  if (computerMove == "Bat") {
-    resultMsg = "It's a tie";
-  } else if (computerMove == "Ball") {
-    resultMsg = "User has won";
-  } else {
-    resultMsg = "Computer has won";
-  }
-};
-
 /*==================== Bat Button Functionality =====================*/
 const batBtn = document.querySelector("#bat-btn");
 
 batBtn.addEventListener("click", () => {
   computerRandomMove();
-  result();
+  let resultMsg;
+  if (computerMove == "Bat") {
+    resultMsg = "It's a tie";
+    score.tie++;
+  } else if (computerMove == "Ball") {
+    resultMsg = "User has won";
+    score.win++;
+  } else {
+    resultMsg = "Computer has won";
+    score.lose++;
+  }
+  localStorage.setItem("score", JSON.stringify(score));
   alert(
-    `You have Chosen Bat. Computer choice is ${computerMove}. ${resultMsg}.`
+    `You have Chosen Bat. Computer choice is ${computerMove}.
+
+  ${resultMsg}.
+
+  ${displayScore()}`
   );
 });
 
@@ -39,9 +55,25 @@ const ballBtn = document.querySelector("#ball-btn");
 
 ballBtn.addEventListener("click", () => {
   computerRandomMove();
-  result();
+
+  let resultMsg;
+  if (computerMove == "Ball") {
+    resultMsg = "It's a tie";
+    score.tie++;
+  } else if (computerMove == "Stump") {
+    resultMsg = "User has won";
+    score.win++;
+  } else {
+    resultMsg = "Computer has won";
+    score.lose++;
+  }
+  localStorage.setItem("score", JSON.stringify(score));
   alert(
-    `You have Chosen Ball. Computer choice is ${computerMove}. ${resultMsg}.`
+    `You have Chosen Ball. Computer choice is ${computerMove}.
+
+    ${resultMsg}.
+    
+    ${displayScore()}`
   );
 });
 
@@ -50,8 +82,40 @@ const stumpBtn = document.querySelector("#stump-btn");
 
 stumpBtn.addEventListener("click", () => {
   computerRandomMove();
-  result();
-  alert(
-    `You have Chosen Stump. Computer choice is ${computerMove}. ${resultMsg}.`
-  );
+
+  let resultMsg;
+  if (computerMove == "Stump") {
+    resultMsg = "It's a tie";
+    score.tie++;
+  } else if (computerMove == "Bat") {
+    resultMsg = "User has won";
+    score.win++;
+  } else {
+    resultMsg = "Computer has won";
+    score.lose++;
+  }
+  localStorage.setItem("score", JSON.stringify(score));
+  document.querySelector(
+    ".heading"
+  ).innerText = `You have Chosen Stump. Computer choice is ${computerMove}. 
+    
+    ${resultMsg}.
+    
+    ${displayScore()}`;
 });
+
+/*==================== Stump Button Functionality =====================*/
+let resetBtn = document.querySelector("#reset-btn");
+
+resetBtn.addEventListener("click", () => {
+  localStorage.clear();
+  score = {
+    win: 0,
+    lose: 0,
+    tie: 0,
+  };
+});
+
+/*==================== Show Display Result =====================*/
+
+function showDisplayResult() {}
